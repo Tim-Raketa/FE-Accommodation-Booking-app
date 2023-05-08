0d503ba33c900dba49525358856408b41bc052cb
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-05-09 01:18:13 +0200

    created registration page

--- src/app/modules/pages/registration/registration.component.ts
+++ src/app/modules/pages/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerUserForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+      surname: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+      jmbg: new FormControl('', [Validators.required, Validators.pattern('[0-9]{13}$')])
+    }, [])
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  get email(){
+    return this.registerUserForm.get('email');
   }
 
+  get password(){
+    return this.registerUserForm.get('password');
+  }
+
+  get confirmPassword(){
+    return this.registerUserForm.get('confirmPassword');
+  }
+
+  get name(){
+    return this.registerUserForm.get('name');
+  }
+
+  get surname(){
+    return this.registerUserForm.get('surname');
+  }
+
+  get jmbg(){
+    return this.registerUserForm.get('jmbg');
+  }
+
+  ngOnInit() {}
+
+  goToLogin =  () => {
+    this.router.navigateByUrl('/login');
+  };
+
+  goToHome =  () => {
+    this.router.navigateByUrl('/home');
+  };
+
+  register =  () => {};
 }
