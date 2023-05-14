4428d11c31bea0cd0f031874ccb3aa10581db119
IsidoraKnezevic <knezevic.ra47.2019@uns.ac.rs>
2023-05-14 18:19:44 +0200

    change to date input and validate dates

--- src/app/modules/pages/create-rentable-interval/create-rentable-interval.component.ts
+++ src/app/modules/pages/create-rentable-interval/create-rentable-interval.component.ts
@@ -21,6 +21,9 @@ export class CreateRentableIntervalComponent implements OnInit {
     automaticAcceptance: new FormControl('', Validators.required),
   })
 
+  error:any={isError:false,errorMessage:''};
+  isValidDate:any;
+
   constructor(private router: Router, private route: ActivatedRoute, private accommodationService: AccommodationService) { }
 
   get startTime() {
@@ -56,6 +59,17 @@ export class CreateRentableIntervalComponent implements OnInit {
   createRentableInterval = () => {
     let startTime = this.createRentableIntervalForm.get("startTime")?.value
     let endTime = this.createRentableIntervalForm.get("endTime")?.value
+
+    console.log(startTime)
+    console.log(typeof(startTime))
+    console.log(endTime)
+    console.log(typeof(endTime))
+
+    let start = new Date(startTime + "T" + "00:00" + ":00");
+    let end = new Date(endTime + "T" + "00:00" + ":00");
+
+    this.isValidDate = this.validateDates(start, end);
+
     let priceOfAccommodation = this.createRentableIntervalForm.get("priceOfAccommodation")?.value
     let pricePerGuest = this.createRentableIntervalForm.get("pricePerGuest")?.value
     let automaticAcceptance = this.createRentableIntervalForm.get("automaticAcceptance")?.value === 'true' ? true : false
@@ -72,15 +86,37 @@ export class CreateRentableIntervalComponent implements OnInit {
 
     console.log(rentableInterval)
 
-    this.accommodationService.createRentableInterval(rentableInterval).subscribe(res =>{
+
+    if(this.isValidDate){
+      this.accommodationService.createRentableInterval(rentableInterval).subscribe(res =>{
         console.log(res);
         this.router.navigate(['host/accommodation', this.accommodationID]);
-      }, error => 
-      {
-        console.log(error)
-      }
-    );
+        }, error => 
+        {
+          console.log(error)
+        }
+      );
+    }
+
+    
 
   };
+
+  validateDates(sDate: Date, eDate: Date){
+    this.isValidDate = true;
+  
+    if((sDate != null && eDate !=null) && (eDate) <= (sDate)){
+      this.error={isError:true,errorMessage:'End date should be greater than start date!'};
+      this.isValidDate = false;
+    }
+
+    let today = new Date();
+    if((sDate <= today) || (eDate <= today)){
+      this.error={isError:true,errorMessage:'Cannot create interval in past!'};
+      this.isValidDate = false;
+    }
+
+    return this.isValidDate;
+  }
 
 }
