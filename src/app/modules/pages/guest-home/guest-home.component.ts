a5552a6d2a07c8c23e60f32f9339ff8b6127a34a
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-05-16 03:15:17 +0200

    update css and transfer reservations to guest-home page

--- src/app/modules/pages/guest-home/guest-home.component.ts
+++ src/app/modules/pages/guest-home/guest-home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../login/auth.service';
+import { ReservationIdsDTO } from '../../model/reservationIdsDTO';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-guest-home',
@@ -8,6 +10,10 @@ import { AuthService } from '../login/auth.service';
   styleUrls: ['./guest-home.component.css']
 })
 export class GuestHomeComponent implements OnInit {
+
+  public dataSource = new MatTableDataSource<ReservationIdsDTO>();
+  public displayedColumns = ['accommodationId', 'numOfGuests', 'startTime','endTime', "delete"];
+  public reservations: ReservationIdsDTO[] = [];
 
   constructor(private router: Router, private authService: AuthService) { }
 
@@ -20,10 +26,9 @@ export class GuestHomeComponent implements OnInit {
 
   logout() {
     this.authService.logout();
-  }
+  }  
 
-  reservations =  () => {
-    this.router.navigateByUrl('/reservations');
-  };
-  
+  cancelReservation(id: number){
+     alert("Trenutno nista ne radi.")
+  }
 }
