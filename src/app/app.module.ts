06caa3eadcc96c05ebc53f6c9a2808089415e74e
Dusan-Stanimirovic <dussanstanimirovic@gmail.com>
2023-06-17 11:48:54 +0200

    started notifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './modules/pages/pages.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';  
+import { MatTableModule } from '@angular/material/table';
 import { AuthInterception } from './modules/pages/login/auth.interceptor';
+import {StompService} from "./modules/services/stompService";
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { AuthInterception } from './modules/pages/login/auth.interceptor';
   providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterception,
-    multi: true}],
+    multi: true},
+    StompService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
